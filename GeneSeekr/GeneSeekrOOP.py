07fbffccc9064c19326f15cfd6fe52f57c3cf06c
mike <mikewknowles@gmail.com>
2015-11-26 16:44:12 -0500

    Fixed argparse import

--- GeneSeekr/GeneSeekrOOP.py
+++ GeneSeekr/GeneSeekrOOP.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 from ARMI_Lt import ARMISeekr
 from Bio.Blast.Applications import NcbiblastnCommandline
+from argparse import ArgumentParser
+import os
 __author__ = 'mike knowles'
 
 
@@ -23,13 +25,11 @@ class GeneSeekr(ARMISeekr):
                                 for hsp in stdout.rstrip().split("\n")]
                     if abs(float(aln[1]) / float(aln[2])) >= self.cutoff/100.0]
 
-if __name__ == '__main__':
-    from argparse import ArgumentParser
-    import os
-    parent = ArgumentParser(add_help=False)
-    parent.add_argument('--version', action='version', version='%(prog)s v0.5')
-    parent.add_argument('-i', '--input', required=True, help='Specify input fasta folder')
-    parent.add_argument('-o', '--output', default=os.getcwd(), help='Specify output folder for csv and json')
-    parent.add_argument('-c', '--cutoff', type=int, help='Threshold for maximum unique bacteria for a single allele')
-    parent.add_argument('-t', '--threads', type=int, default=12, help='Specify number of threads')
-    parent.add_argument('-m', '--marker', required=True, help='Specify gene file in FASTA format')
+
+parent = ArgumentParser(add_help=False)
+parent.add_argument('--version', action='version', version='%(prog)s v0.5')
+parent.add_argument('-i', '--input', required=True, help='Specify input fasta folder')
+parent.add_argument('-o', '--output', default=os.getcwd(), help='Specify output folder for csv and json')
+parent.add_argument('-c', '--cutoff', type=int, help='Threshold for maximum unique bacteria for a single allele')
+parent.add_argument('-t', '--threads', type=int, default=12, help='Specify number of threads')
+parent.add_argument('-m', '--marker', required=True, help='Specify gene file in FASTA format')
