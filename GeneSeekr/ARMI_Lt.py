240cda019fa24f13193d56a64ecba7f85a8ea23b
mike <mikewknowles@gmail.com>
2015-12-11 13:41:55 -0500

    signal tests

--- GeneSeekr/ARMI_Lt.py
+++ GeneSeekr/ARMI_Lt.py
@@ -140,7 +140,6 @@ class ARMISeekr(object):
         try:
             return self._blast(data)
         except KeyboardInterrupt:
-            signal.signal(signal.SIGINT, signal.SIG_IGN)
             raise KeyboardInterruptError()
 
     def mpblast(self, cutoff=70):
@@ -161,9 +160,9 @@ class ARMISeekr(object):
                             self.plus[fasta][gene].sort()
             except KeyboardInterrupt:
                 print "[{0:s}] Got ^C while pool mapping, terminating the pool".format(time.strftime("%H:%M:%S"))
-                # p.terminate()
+                p.terminate()
                 print '[{0:s}] Pool is terminated'.format(time.strftime("%H:%M:%S"))
-                sys.exit(0)
+                sys.exit(127)
             except Exception, e:
                 print "[{0:s}] Got exception: {1!r:s}, terminating the pool".format(e, time.strftime("%H:%M:%S"))
                 p.terminate()
