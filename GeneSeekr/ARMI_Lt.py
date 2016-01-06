f5578244958214832c180137287b775e18b18d8f
mike <mikewknowles@gmail.com>
2016-01-06 14:50:14 -0500

    Parallelized parsing

--- GeneSeekr/ARMI_Lt.py
+++ GeneSeekr/ARMI_Lt.py
@@ -142,7 +142,7 @@ class ARMISeekr(object):
         genelist, plus = list(), defaultdict(list)
         try:
             fasta = data[0]
-            for fastaline, n in self._blast(data):
+            for fastaline in self._blast(data):
                 print fastaline
                 if fastaline is not None:  # if the returned list contains [genome, gene, value]
                     for sgenes, values in fastaline:  # unpack
