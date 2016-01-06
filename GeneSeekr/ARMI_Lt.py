e31e4f39bfe37fa10672c10d9cd6c728a50b8176
mike <mikewknowles@gmail.com>
2016-01-06 15:38:49 -0500

    Parallelized parsing

--- GeneSeekr/ARMI_Lt.py
+++ GeneSeekr/ARMI_Lt.py
@@ -145,8 +145,9 @@ class ARMISeekr(object):
             fasta = data[0]
             result = self._blast(data)
             plus[fasta] = defaultdict(list)
-            for sgenes, values in result:
-                if (sgenes, values) is not (None, None):
+            for fastaline in result:
+                if fastaline is not None:
+                    sgenes, values = fastaline
                     for gene in sgenes:
                         genelist.add(gene)  # create set of all genes in analysis
                         plus[fasta][gene].append(values)
