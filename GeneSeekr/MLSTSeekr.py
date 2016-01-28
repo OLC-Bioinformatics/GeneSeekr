1ba1c532e2a7f0b10c92ec38c8869d7116480b0f
mike <mikewknowles@gmail.com>
2016-01-28 12:06:13 -0500

    fixed general mlst

--- GeneSeekr/MLSTSeekr.py
+++ GeneSeekr/MLSTSeekr.py
@@ -18,13 +18,11 @@ class GeneSeekr(ARMISeekr):
                                        num_alignments=10000)
         stdout, stderr = blastn()
         if stdout != '':
-            test =  [[[gene], [int(allele), qacc]]
+            return [[[gene], [int(allele), qacc]]
                     for sseqid, nident, slen, qacc in [hsp.split('\t')
                     for hsp in stdout.rstrip().split("\n")]
                     for gene, allele in [sseqid.split('_')]
                     if abs(float(nident) / float(slen)) >= self.cutoff/100.0]
-            print test
-            return test
 
 
 parent = ArgumentParser(add_help=False)
