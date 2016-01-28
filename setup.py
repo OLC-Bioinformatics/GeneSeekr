a715336b8011cb86fc40a48f4afb3445ff4e0bb6
mike <mikewknowles@gmail.com>
2016-01-28 15:56:02 -0500

    Using local card db generation

--- setup.py
+++ setup.py
@@ -12,8 +12,7 @@ class build_card(install):
     description = 'download CARD fasta and modify for ARMI'
     def run(self):
         db = os.path.abspath(os.path.join(os.path.split(__file__)[0], 'Geneseekr', 'data', 'ARMI-genes.dat'))
-        print db
-        if not db:
+        if not os.path.isfile(db):
             makedb(db)
         install.run(self)
 
