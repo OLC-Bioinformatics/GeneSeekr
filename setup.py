2db8db147250a111adb95039df109a83547d522b
mike <mikewknowles@gmail.com>
2016-01-28 15:49:58 -0500

    Using local card db generation

--- setup.py
+++ setup.py
@@ -1,37 +1,21 @@
 try:
     from setuptools import setup, Command
-    from setuptools.command.bdist_egg import bdist_egg as _bdist_egg
+    from setuptools.command.install import install
 except ImportError:
     from distutils.core import setup, Command
-    from distutils.command.bdist import bdist as _bdist_egg
-from distutils.command.build import build as build_module
+    from distutils.command.install import install
 from GeneSeekr.data import makedb
 import os
 
 
-class bdist_egg(_bdist_egg):
-    def run(self):
-        self.run_command('Build')
-        _bdist_egg.run(self)
-
-class build_card(Command):
+class build_card(install):
     description = 'download CARD fasta and modify for ARMI'
-    user_options = []
-
-    def initialize_options(self):
-        pass
-
-    def finalize_options(self):
-        pass
-
     def run(self):
         db = os.path.join(os.path.split(__file__)[0], 'Geneseekr', 'data', 'ARMI-genes.dat')
         if not db:
             makedb(db)
-        build_module.build.run(self)
+        install.build.run(self)
 
-class build(build_module):
-    sub_commands = build_module.sub_commands + [('build_card', None)]
 
 setup(
     name='pythonGeneSeekr',
@@ -47,9 +31,7 @@ setup(
     long_description=open('README.md').read(),
     install_requires=['biopython >= 1.65',
                       'argparse >= 1.4.0'],
-    cmdclass={'bdist_egg': bdist_egg,
-              'build': build,
-              'build_card': build_card},
+    cmdclass={'install': build_card},
     scripts=['bin/ARMI',
              'bin/MLSTSeekr',
              'bin/GeneSeekr',
