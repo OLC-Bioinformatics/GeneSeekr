01022dc6246c6f34f1fe289c3ca9273b8a4a9537
mike <mikewknowles@gmail.com>
2016-01-28 14:36:49 -0500

    Using local card db generation

--- GeneSeekr/data/__init__.py
+++ GeneSeekr/data/__init__.py
@@ -1,2 +1,44 @@
 #!/usr/bin/env python
+from gzip import GzipFile
+from StringIO import StringIO
+from collections import defaultdict
+import urllib
 __author__ = 'mike knowles'
+
+
+def dlunzip(url):
+    source = urllib.urlopen(url).read()
+    compressedFile = StringIO()
+    compressedFile.write(source)
+    #
+    # Set the file's current position to the beginning
+    # of the file so that gzip.GzipFile can read
+    # its contents from the top.
+    #
+    compressedFile.seek(0)
+    decompressedFile = GzipFile(fileobj=compressedFile, mode='rb')
+    return decompressedFile
+
+def makedb(path):
+    genelist = defaultdict(list)
+    for line in dlunzip('http://arpcard.mcmaster.ca/blast/db/AROtags.txt.gz'):
+        acc, name, aro = line.rstrip().split("\t")
+        gene = ".".join(acc.split(".")[:-1])
+        genelist[gene].append(aro[4:])
+    handle = open(path, 'w')
+    for line in dlunzip('http://arpcard.mcmaster.ca/blast/db/nucleotide/ARmeta-genes.fa.gz'):
+        if ">" == line[0]:
+            try:
+                aro = "".join(genelist[line.split(" ")[0][1:]])
+            except KeyError:
+                aro = line.split("ARO:")[-1][:7]
+                if aro == "1000001":
+                    aro = line.split("ARO:")[1][:7]
+            handle.write(">ARO:%s %s" % (aro, line[1:]))
+        else:
+            handle.write(line)
+    handle.close()
+    print 'installing card database'
+if __name__ == '__main__':
+    import os
+    makedb(os.path.join(os.getcwd(), 'ARMI-genes.dat'))
--- setup.py
+++ setup.py
@@ -2,12 +2,22 @@ try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
+from distutils.command import build as build_module
+from GeneSeekr.data import makedb
+import os
+
+
+class Build(build_module.build):
+    def run(self):
+        if not os.path.join(os.getcwd(), 'Geneseekr', 'data', 'ARMI-genes.dat'):
+            makedb(os.path.join(os.getcwd(), 'Geneseekr', 'data', 'ARMI-genes.dat'))
+        build_module.build.run(self)
 
 setup(
     name='pythonGeneSeekr',
     version='0.5dev2',
     packages=['GeneSeekr'],
-    package_data={'': ['GeneSeekr/data/aro.dat']},
+    package_data={'': ['GeneSeekr/data/*.dat']},
     include_package_data=True,
     url='https://github.com/OLC-Bioinformatics/pythonGeneSeekr',
     license='MIT',
@@ -17,7 +27,9 @@ setup(
     long_description=open('README.md').read(),
     install_requires=['biopython >= 1.65',
                       'argparse >= 1.4.0'],
+    cmdclass={'build': Build},
     scripts=['bin/ARMI',
              'bin/MLSTSeekr',
-             'bin/GeneSeekr']
+             'bin/GeneSeekr',
+             'bin/resfinder']
 )
