745b25ae0d4b227ea2c5a7baeb2be9dfd8c054db
Adam Koziol <adam.koziol@inspection.gc.ca>
2018-07-24 16:26:18 -0400

    Initial commit. Massive overhaul to GeneSeekr logic

---
+++ bin/blastn.py
@@ -0,0 +1,117 @@
+#!/usr/bin/env python3
+from accessoryFunctions.accessoryFunctions import printtime
+from methods.geneseekr import GeneSeekr, Parser, sequencenames
+import multiprocessing
+
+
+class BLASTn(object):
+
+    def geneseekr(self):
+        printtime('Performing {program} analyses on {at} targets'
+                  .format(program=self.program,
+                          at=self.analysistype),
+                  self.start)
+        # Create the GeneSeekr object
+        geneseekr = GeneSeekr()
+        # Make blast databases (if necessary)
+        printtime('Creating {at} blast databases as required'
+                  .format(at=self.analysistype),
+                  self.start)
+        geneseekr.makeblastdb(self.combinedtargets)
+        # Populate variables
+        self.targetfolders, self.targetfiles, self.records = geneseekr.target_folders(self.metadata,
+                                                                                      self.analysistype)
+        printtime('Performing {program} analyses on {at} targets'
+                  .format(program=self.program,
+                          at=self.analysistype),
+                  self.start)
+        self.metadata = geneseekr.run_blast(self.metadata,
+                                            self.analysistype,
+                                            self.program,
+                                            self.outfmt,
+                                            evalue=self.evalue,
+                                            num_threads=self.cpus)
+        # Parse the output depending on whether unique results are desired
+        printtime('Parsing {program} results for {at} targets'
+                  .format(program=self.program,
+                          at=self.analysistype),
+                  self.start)
+        if self.unique:
+            # Run the unique blastn parsing module
+            self.metadata = geneseekr.unique_parse_blast(self.metadata,
+                                                         self.analysistype,
+                                                         self.fieldnames,
+                                                         self.cutoff,
+                                                         self.program)
+            # Filter the unique hits
+            self.metadata = geneseekr.filter_unique(self.metadata,
+                                                    self.analysistype)
+        else:
+            # Run the standard blastn parsing module
+            self.metadata = geneseekr.parse_blastn(self.metadata,
+                                                   self.analysistype,
+                                                   self.fieldnames,
+                                                   self.cutoff)
+        # Create reports
+        printtime('Creating {at} reports'.format(at=self.analysistype), self.start)
+        if self.analysistype == 'resfinder':
+            # ResFinder-specific report
+            self.metadata = geneseekr.resfinder_reporter(self.metadata,
+                                                         self.analysistype,
+                                                         self.targetfolders,
+                                                         self.reportpath,
+                                                         self.align,
+                                                         self.targetfiles,
+                                                         self.records)
+        elif self.analysistype == 'virulence':
+            # VirulenceFinder-specific report
+            geneseekr.virulencefinder_reporter(self.metadata,
+                                               self.analysistype,
+                                               self.reportpath)
+        else:
+            # GeneSeekr-specific report
+            self.metadata = geneseekr.reporter(self.metadata,
+                                               self.analysistype,
+                                               self.reportpath,
+                                               self.align,
+                                               self.targetfiles,
+                                               self.records)
+        # Remove the attributes from the object; they take up too much room on the .json report
+        for sample in self.metadata:
+            delattr(sample[self.analysistype], "targetnames")
+            delattr(sample[self.analysistype], "targets")
+        printtime('{at} analyses complete'.format(at=self.analysistype), self.start)
+
+    def __init__(self, args):
+        self.cutoff = args.cutoff
+        # Use the argument for the number of threads to use, or default to the number of cpus in the system
+        self.cpus = args.numthreads if args.numthreads else multiprocessing.cpu_count() - 1
+        self.align = args.align
+        self.resfinder = args.resfinder
+        self.virulencefinder = args.virulencefinder
+        # Automatically set self.unique to true for ResFinder or VirulenceFinder analyses
+        self.unique = True if self.resfinder or self.virulencefinder else args.unique
+        self.analysistype = args.analysistype
+        self.start = args.start
+        self.evalue = args.evalue
+        self.program = args.program
+        # Run the Parser class from the geneseekr methods script to create lists of the database targets, and
+        # combined targets, fasta sequences, and metadata objects.
+        parse = Parser(self, args)
+        parse.strainer()
+        # Extract the variables from the object
+        self.reportpath = parse.reportpath
+        self.targets = parse.targets
+        self.strains = parse.strains
+        self.combinedtargets = parse.combinedtargets
+        self.metadata = parse.metadata
+        # Fields used for custom outfmt 6 BLAST output:
+        self.fieldnames = ['query_id', 'subject_id', 'positives', 'mismatches', 'gaps',
+                           'evalue', 'bit_score', 'subject_length', 'alignment_length',
+                           'query_start', 'query_end', 'query_sequence',
+                           'subject_start', 'subject_end', 'subject_sequence']
+        self.outfmt = "'6 qseqid sseqid positive mismatch gaps " \
+                      "evalue bitscore slen length qstart qend qseq sstart send sseq'"
+        self.targetfolders = set()
+        self.targetfiles = list()
+        self.records = dict()
--- setup.py
+++ setup.py
@@ -1,99 +1,18 @@
-try:
-    from setuptools import setup, Command
-    from setuptools.command.install import install
-except ImportError:
-    from distutils.core import setup, Command
-    from distutils.command.install import install
-import sys
-import os
+#!/usr/bin/env python
 
-
-class UpdateDB(install):
-    description = 'update CARD ontology'
-    user_options = install.user_options + [
-        ('card-version=', None, 'Specify the version of CARD to download (default 1.0.3)'),
-        ('card-url=', None, 'URL of CARD to download, this should only be used if card_version doesn\'t work')
-    ]
-
-    def run(self, obj=None):
-        from GeneSeekr.data import Build
-        card = Build(self.card_url)
-        card.updatearo(os.path.join(os.path.split(__file__)[0], 'GeneSeekr', 'data', 'aro.dat'))
-        card.makedb(os.path.join(os.path.split(__file__)[0], 'GeneSeekr', 'data', 'genes.dat'))
-        del card
-        # Attempt to detect whether we were called from setup() or by another
-        # command.  If we were called by setup(), our caller will be the
-        # 'run_command' method in 'distutils.dist', and *its* caller will be
-        # the 'run_commands' method.  If we were called any other way, our
-        # immediate caller *might* be 'run_command', but it won't have been
-        # called by 'run_commands'.  This is slightly kludgy, but seems to
-        # work.
-        #
-        caller = sys._getframe(2)
-        caller_module = caller.f_globals.get('__name__', '')
-        caller_name = caller.f_code.co_name
-        if caller_module != 'distutils.dist' or caller_name != 'run_commands':
-            # We weren't called from the command line or setup(), so we
-            # should run in backward-compatibility mode to support bdist_*
-            # commands.
-            install.run(self)
-        else:
-            self.do_egg_install()
-
-    def initialize_options(self):
-        install.initialize_options(self)
-        self.card_version = None
-        self.card_url = None
-
-    def finalize_options(self):
-        install.finalize_options(self)
-        self.card_version = self.card_version if self.card_version else "1.0.4"
-        if self.card_version and not self.card_url:
-            assert self.card_version != '1.0.0', 'Version 1.0.0 not supported'
-            assert "." in self.card_version, 'Invalid Card Version {0!r:s}'
-            assert all(map(str.isdigit, self.card_version.split("."))), 'Invalid Card Version {0!r:s}'
-            self.card_url = 'https://card.mcmaster.ca/download/0/broadsteet-v{0:s}.tar.gz'.format(self.card_version)
-        elif self.card_url:
-            assert self.card_url.startswith("http"), "URL must start with http "
-        import urllib2
-        request = urllib2.Request(self.card_url)
-        request.get_method = lambda: 'HEAD'
-        try:
-            response = urllib2.urlopen(request)
-            assert int(response.getcode()) < 400, \
-                'HTTP Error {0:s}: ({1:s})'.format(response.response.getcode(), self.card_url)
-            assert response.info().type != 'text/html', 'URL {0:s} points to webpage not file'.format(self.card_url)
-        except urllib2.HTTPError as e:
-            print 'HTTP Error {0:s}: {1:s} ({2:s})'.format(e.code, e.msg, e.url)
-            raise
-        except:
-            print 'Malformed CARD URL Error'
-            print u'If you do did not specify the URL:\'{0:s}\' or version:\'{1:s}\' then either CARD is down or ' \
-                  u'the link has changed, you may be able to remedy this by specifying a url or version with ' \
-                  u'--card-version=\'version\' or  --card-url=\'url\' after \'{2:s}\''\
-                .format(self.card_url, self.card_version, ' '.join(sys.argv))
-            raise
+from setuptools import setup, find_packages
 
 setup(
-    name='GeneSeekr',
-    version='0.6.dev1',
-    packages=['GeneSeekr'],
-    package_data={'': ['GeneSeekr/data/*.dat']},
-    include_package_data=True,
-    url='https://github.com/OLC-Bioinformatics/GeneSeekr',
-    license='MIT',
-    author='mike knowles',
-    author_email='mikewknowles@gmail.com',
-    description='BLAST formatter for full genes',
-    long_description=open('README.md').read(),
-    setup_requires=['setuptools >= 18.0.1'],
-    install_requires=['biopython >= 1.65',
-                      'argparse >= 1.4.0',
-                      'pysam == 0.8.4',
-                      'pysamstats'],
-    cmdclass=dict(install=UpdateDB, nocard=install),
-    scripts=['bin/ARMI',
-             'bin/MLSTSeekr',
-             'bin/GeneSeekr',
-             'bin/resfinder']
+    name="GeneSeekr",
+    version="0.0.1",
+    packages=find_packages(),
+    scripts=['methods/geneseekr',
+	     'bin/blastn'],
+    author="Adam Koziol",
+    author_email="adam.koziol@canada.ca",
+    url="https://github.com/OLC-Bioinformatics/GeneSeekr",
+    install_requires=['olctools', 
+		      'biopython',
+		      'xlsxwriter
+	]
 )
