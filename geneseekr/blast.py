fb657a93edcc152c40c4f2c9f58580bcaf172e97
adamkoziol <adam.koziol@inspection.gc.ca>
2018-10-31 13:36:41 -0400

    Updates to BLAST class to allow for additional analyses e.g. GDCS, MLST, etc.

--- geneseekr/blast.py
+++ geneseekr/blast.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
+from accessoryFunctions.accessoryFunctions import MetadataObject
+from MLSTsippr.mlst import GeneSippr as MLSTSippr
 from geneseekr.geneseekr import GeneSeekr
 from geneseekr.parser import Parser
 import multiprocessing
+from glob import glob
 import logging
 import os
 
@@ -29,9 +32,6 @@ class BLAST(object):
         for sample in self.metadata:
             self.geneseekr.makeblastdb(sample[self.analysistype].combinedtargets,
                                        self.program)
-            self.targetfolders, self.targetfiles, self.records = \
-                self.geneseekr.target_folders(self.metadata,
-                                              self.analysistype)
 
     def run_blast(self):
         """
@@ -39,12 +39,38 @@ class BLAST(object):
         """
         logging.info('Performing {program} analyses on {at} targets'.format(program=self.program,
                                                                             at=self.analysistype))
-        self.metadata = self.geneseekr.run_blast(self.metadata,
-                                                 self.analysistype,
-                                                 self.program,
-                                                 self.outfmt,
-                                                 evalue=self.evalue,
-                                                 num_threads=self.cpus)
+        if 'mlst' in self.analysistype.lower():
+            self.metadata = self.geneseekr.run_blast(self.metadata,
+                                                     self.analysistype,
+                                                     self.program,
+                                                     self.outfmt,
+                                                     evalue='1E-20',
+                                                     num_threads=self.cpus,
+                                                     perc_identity=99)
+        elif 'sixteens' in self.analysistype:
+            self.metadata = self.geneseekr.run_blast(self.metadata,
+                                                     self.analysistype,
+                                                     self.program,
+                                                     self.outfmt,
+                                                     evalue='1E-100',
+                                                     num_threads=self.cpus,
+                                                     num_alignments=1000,
+                                                     perc_identity=98)
+        elif self.analysistype == 'GDCS':
+            self.metadata = self.geneseekr.run_blast(self.metadata,
+                                                     self.analysistype,
+                                                     self.program,
+                                                     self.outfmt,
+                                                     evalue=self.evalue,
+                                                     num_threads=self.cpus,
+                                                     task='blastn')
+        else:
+            self.metadata = self.geneseekr.run_blast(self.metadata,
+                                                     self.analysistype,
+                                                     self.program,
+                                                     self.outfmt,
+                                                     evalue=self.evalue,
+                                                     num_threads=self.cpus)
 
     def parse_results(self):
         """
@@ -52,23 +78,29 @@ class BLAST(object):
         """
         logging.info('Parsing {program} results for {at} targets'.format(program=self.program,
                                                                          at=self.analysistype))
-        if self.unique:
+        if 'sixteens' in self.analysistype:
+            self.metadata = self.geneseekr.sixteens_parser(metadata=self.metadata,
+                                                           analysistype=self.analysistype,
+                                                           fieldnames=self.fieldnames,
+                                                           cutoff=self.cutoff,
+                                                           program=self.program)
+        elif self.unique:
             # Run the unique blast parsing module
-            self.metadata = self.geneseekr.unique_parse_blast(self.metadata,
-                                                              self.analysistype,
-                                                              self.fieldnames,
-                                                              self.cutoff,
-                                                              self.program)
+            self.metadata = self.geneseekr.unique_parse_blast(metadata=self.metadata,
+                                                              analysistype=self.analysistype,
+                                                              fieldnames=self.fieldnames,
+                                                              cutoff=self.cutoff,
+                                                              program=self.program)
             # Filter the unique hits
-            self.metadata = self.geneseekr.filter_unique(self.metadata,
-                                                         self.analysistype)
+            self.metadata = self.geneseekr.filter_unique(metadata=self.metadata,
+                                                         analysistype=self.analysistype)
         else:
             # Run the standard blast parsing module
-            self.metadata = self.geneseekr.parse_blast(self.metadata,
-                                                       self.analysistype,
-                                                       self.fieldnames,
-                                                       self.cutoff,
-                                                       self.program)
+            self.metadata = self.geneseekr.parse_blast(metadata=self.metadata,
+                                                       analysistype=self.analysistype,
+                                                       fieldnames=self.fieldnames,
+                                                       cutoff=self.cutoff,
+                                                       program=self.program)
 
     def create_reports(self):
         """
@@ -94,6 +126,54 @@ class BLAST(object):
             self.geneseekr.virulencefinder_reporter(self.metadata,
                                                     self.analysistype,
                                                     self.reportpath)
+        elif 'mlst' in self.analysistype.lower():
+            # Adjust the analysis type to be consistently lowercase
+            self.analysistype = self.analysistype.lower()
+            # Create the necessary attributes for the MLST-typing method
+            for sample in self.metadata:
+                sample[self.analysistype].alleles = sorted(list(set(allele.split('_')[0]
+                                                                    for allele in sample[self.analysistype]
+                                                                    .targetnames)))
+                # In order to work with the Enterobase cgMLST scheme that has underscores in the gene names (e.g.
+                # AEJV01_03887, check for multiple underscores in the allele name, and act appropriately
+                if len(sample[self.analysistype].alleles) > 53:
+                    allele_set = set()
+                    for allele in sample[self.analysistype].targetnames:
+                        if len(allele.split('_')) == 3:
+                            allele = '_'.join([allele.split('_')[0], allele.split('_')[1]])
+                        else:
+                            allele = allele.split('_')[0]
+                        allele_set.add(allele)
+                    sample[self.analysistype].alleles = sorted(list(allele_set))
+                # Allele names attribute is apparently the same as the alleles attribute
+                sample[self.analysistype].allelenames = sample[self.analysistype].alleles
+                try:
+                    sample[self.analysistype].profile = glob(os.path.join(sample[self.analysistype].targetpath,
+                                                                          '*.txt'))[0]
+                except IndexError:
+                    sample[self.analysistype].profile = 'NA'
+            # Create the typing object
+            typing = MLST(args=self,
+                          pipelinecommit='',
+                          startingtime=self.start,
+                          scriptpath='',
+                          analysistype=self.analysistype,
+                          cutoff=0.99,
+                          pipeline=self.pipeline)
+            # Perform typing, and create reports
+            typing.reporter()
+        elif 'sixteens' in self.analysistype:
+            self.metadata = self.geneseekr.sixteens_reporter(metadata=self.metadata,
+                                                             analysistype=self.analysistype,
+                                                             reportpath=self.reportpath)
+        elif self.analysistype == 'GDCS':
+            self.metadata = self.geneseekr.gdcs_reporter(metadata=self.metadata,
+                                                         analysistype=self.analysistype,
+                                                         reportpath=self.reportpath)
+        elif 'sero' in self.analysistype:
+            self.metadata = self.geneseekr.sero_reporter(metadata=self.metadata,
+                                                         analysistype=self.analysistype,
+                                                         reportpath=self.reportpath)
         else:
             # GeneSeekr-specific report
             self.metadata = self.geneseekr.reporter(self.metadata,
@@ -112,7 +192,8 @@ class BLAST(object):
         self.metadata = self.geneseekr.clean_object(self.metadata,
                                                     self.analysistype)
 
-    def __init__(self, args, analysistype='geneseekr', cutoff=70, program='blastn', genus_specific=False):
+    def __init__(self, args, analysistype='geneseekr', cutoff=70, program='blastn', genus_specific=False, unique=False,
+                 evalue='1E-05', pipeline=True):
         try:
             args.program = args.program
         except AttributeError:
@@ -132,12 +213,14 @@ class BLAST(object):
             self.align = True
         if analysistype == 'geneseekr':
             try:
-                self.analysistype = args.analysistype
+                self.analysistype = args.analysistype.lower()
             except AttributeError:
-                self.analysistype = analysistype
-                args.analysistype = analysistype
-        else:
+                self.analysistype = analysistype.lower()
+                args.analysistype = analysistype.lower()
+        elif analysistype == 'GDCS':
             self.analysistype = analysistype
+        else:
+            self.analysistype = analysistype.lower()
         try:
             self.resfinder = args.resfinder
         except AttributeError:
@@ -146,9 +229,18 @@ class BLAST(object):
             self.virulencefinder = args.virulencefinder
         except AttributeError:
             self.virulencefinder = False
-        # Automatically set self.unique to true for ResFinder or VirulenceFinder analyses
-        self.unique = True if self.resfinder or self.virulencefinder or 'resfinder' in self.analysistype \
-            or self.analysistype == 'virulencefinder' else args.unique
+        try:
+            self.typing = args.typing
+        except AttributeError:
+            self.typing = False
+        # Automatically set self.unique to true for the appropriate analyses
+        try:
+            self.unique = True if unique or self.resfinder is True or self.virulencefinder is True or 'resfinder' in \
+                                  self.analysistype or 'virulence' in self.analysistype or self.typing is True \
+                                  or 'mlst' in self.analysistype.lower() or 'sixteens' in self.analysistype \
+                                  else args.unique
+        except AttributeError:
+            self.unique = unique
         try:
             self.start = args.start
         except AttributeError:
@@ -156,7 +248,7 @@ class BLAST(object):
         try:
             self.evalue = args.evalue
         except AttributeError:
-            self.evalue = '1E-05'
+            self.evalue = evalue
         try:
             self.sequencepath = args.sequencepath
         except AttributeError:
@@ -167,6 +259,7 @@ class BLAST(object):
             self.targetpath = args.targetpath
         self.reportpath = args.reportpath
         self.genus_specific = genus_specific
+        self.pipeline = pipeline
         try:
             self.metadata = args.runmetadata.samples
             parse = Parser(self)
@@ -196,3 +289,35 @@ class BLAST(object):
         self.records = dict()
         # Create the GeneSeekr object
         self.geneseekr = GeneSeekr()
+        # Class variables required to use MLST
+        self.path = self.sequencepath
+        self.logfile = os.path.join(self.path, 'log')
+        self.runmetadata = MetadataObject()
+        self.runmetadata.samples = self.metadata
+
+
+class MLST(MLSTSippr):
+    def reporter(self):
+        analysistype = 'rmlst' if 'rmlst' in self.analysistype.lower() else 'mlst'
+        # Populate self.plusdict in order to reuse parsing code from an assembly-based method
+        for sample in self.runmetadata.samples:
+            if sample.general.bestassemblyfile != 'NA':
+                for gene in sample[analysistype].allelenames:
+                    for allele, percentidentity in sample[analysistype].blastresults.items():
+                        if gene in allele:
+                            # Split the allele number from the gene name using the appropriate delimiter
+                            if '_' in allele:
+                                splitter = '_'
+                            elif '-' in allele:
+                                splitter = '-'
+                            else:
+                                splitter = ''
+                            # Create the plusdict dictionary as in the assembly-based (r)MLST method. Allows all the
+                            # parsing and sequence typing code to be reused.
+                            try:
+                                self.plusdict[sample.name][gene][allele.split(splitter)[-1]][percentidentity] = 10
+                            except IndexError:
+                                pass
+        self.profiler()
+        self.sequencetyper()
+        self.mlstreporter()
