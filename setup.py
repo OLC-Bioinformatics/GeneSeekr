079854a5c94762d0a7950d5cd8bf71d4bb33d121
adamkoziol <adam.koziol@inspection.gc.ca>
2019-05-23 15:47:59 -0400

    Restructuring package

--- setup.py
+++ setup.py
@@ -1,20 +1,14 @@
-#!/usr/bin/env python
-
 from setuptools import setup, find_packages
+import os
 
 setup(
     name="geneseekr",
-    version="0.3.4",
+    version="0.3.5",
     packages=find_packages(),
-    scripts=['GeneSeekr'],
+    scripts=[
+        os.path.join('geneseekr', 'GeneSeekr')
+    ],
     author="Adam Koziol",
     author_email="adam.koziol@canada.ca",
     url="https://github.com/OLC-Bioinformatics/GeneSeekr",
-    install_requires=[
-        'biopython',
-        'click',
-        'numpy',
-        'olctools',
-        'xlsxwriter',
-        'pytest']
 )
